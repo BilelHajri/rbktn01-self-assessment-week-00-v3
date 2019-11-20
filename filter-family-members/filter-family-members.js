8f8b0a60c06a8da1c5db57bc6f209af838267213
bilel <hb.bilel@gmail.com>
2019-11-20 11:51:13 +0100

    unfinished

--- filter-family-members/filter-family-members.js
+++ filter-family-members/filter-family-members.js
@@ -1,4 +1,4 @@
-// `filterFamilyMembers` accepts two arguments, a family tree object, and a truth test. `filterFamilyMembers` 
+// `filterFamilyMembers` accepts two arguments, a family tree object, and a truth test. `filterFamilyMembers`
 // returns an array, in any order, of the full names of family members who pass the passed in truth test.
 // You will need to use recursion in your solution in order to handle nested family trees.
 //
@@ -17,7 +17,7 @@
 //   'firstName': 'Beth',
 //   'lastName': 'Johnson',
 //   'location': 'San Francisco',
-//   'children': [
+//   'children': [ // 2 element
 //     {
 //       'firstName': 'Beth Jr.',
 //       'lastName': 'Johnson',
@@ -52,5 +52,27 @@
 
 var filterFamilyMembers = function (familyTree, truthTest) {
   // All your code in this function body
+  var result = [] || result;
+
+  if (livesInBerkeley(familyTree.location)) {
+    result.push(familyTree.firstName +'.' +familyTree.lastName)
+  }
+
+  if (familyTree.children[0] !== undefined) {
+
+    familyTree.children.forEach(function (el) {
+
+        filterFamilyMembers(el,truthTest,result)
+
+    })
+
+  /* result.push(familyTree.children[0].firstName +'.' +familyTree.children[0].lastName) */
+
+  }
+
+  return result;
 };
+
+
+//function return arr of famliy memebre from tree
 
